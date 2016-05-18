baf4c77643d877394d7bb15857310d861c33b5a5
Jason <zhanyouwei@meitunmama.com>
2016-05-18 15:08:29 +0800

    更新DEMO

--- docs/demo.js
+++ docs/demo.js
@@ -3,7 +3,19 @@
  * Created on 16/5/17.
  */
 
-define([], function () {
-	'use strict';
+$(function () {
+  'use strict';
+  var typeArr = ['success', 'warning', 'danger', 'secondary'];
+  showDemo();
+  setInterval(function () {
+    showDemo();
+  }, 3000);
 
+  function showDemo() {
+    $('body').toast({
+      text: 'Amaze UI Toast',
+      duration: 2000,
+      type: typeArr[Math.floor(Math.random() * 4)]
+    });
+  }
 });
